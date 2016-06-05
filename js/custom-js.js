168e15ff321c50ebe7013a146a8f0bac6458b4b8
Lizz Hale <lizzhale@github.com>
2016-06-05 09:45:29 -0700

    Use imagesloaded to prevent grid overlap

--- js/custom-js.js
+++ js/custom-js.js
@@ -3,6 +3,10 @@ var $grid = $('.grid').masonry({
   columnWidth: 160
 });
 
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
+
 $grid.on( 'click', '.grid-item', function() {
   // change size of item via class
   $( this ).toggleClass('grid-item--gigante');
