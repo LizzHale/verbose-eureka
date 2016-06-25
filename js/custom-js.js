5e4c3f2ce26b8e3c0818c26bef5357b48a979ae9
Lizz Hale <lizzhale@github.com>
2016-06-25 16:37:17 -0700

    Set custom delay for toolbar

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+  $('#lightgallery').lightGallery({
+    hideBarsDelay: 600,
+  });
+});
 
 $(window).load(function(){ $('.grid').masonry({
     itemSelector: '.grid-item'
