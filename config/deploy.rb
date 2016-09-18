536f7bfd5e472714e32cfc6c59b7c6052e3eedd5
Lizz Hale <lizzhale@github.com>
2016-09-17 18:13:06 -0700

    update deploy to

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'my_app_name'
 set :repo_url, 'git@github.com:lizzhale/verbose-eureka.git'
 
 set :branch, :master
-set :deploy_to, '/home/deploy/contactbook'
+set :deploy_to, '/home/deploy/verbose-eureka'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
