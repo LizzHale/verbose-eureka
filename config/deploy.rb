0a0c7d9aaa91177ad610a34551371e2c633b30e8
Lizz Hale <lizzhale@github.com>
2016-09-17 18:14:07 -0700

    Update branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.1'
 set :application, 'my_app_name'
 set :repo_url, 'git@github.com:lizzhale/verbose-eureka.git'
 
-set :branch, :master
+set :branch, :app
 set :deploy_to, '/home/deploy/verbose-eureka'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
