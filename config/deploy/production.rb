41ca8141e5986511413a552ad2d0970c8e1741ee
Lizz Hale <lizzhale@github.com>
2016-09-19 00:05:43 -0700

    New ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server '54.172.186.109', user: 'deploy', roles: %w{web app db}
+server '52.45.159.27', user: 'deploy', roles: %w{web app db}
 
 
 # role-based syntax
