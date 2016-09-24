d747c266c21d599ef7c770c911a3cf4e9ec3877f
Lizz Hale <lizzhale@github.com>
2016-09-23 17:38:38 -0700

    Remove bin from capistrano's linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :branch, :app
 set :deploy_to, '/home/deploy/verbose-eureka'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.2.0' # Edit this if you are using MRI Ruby
