8f2e147b1f51e3d1b5a5e2078c0feef3930bb314
Lizz Hale <lizzhale@github.com>
2016-09-23 21:34:24 -0700

    Load jquery turbolinks before turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require turbolinks
 //= require bootstrap.min
 //= require jquery.mousewheel.min
@@ -26,4 +27,3 @@
 //= require masonry.pkgd
 //= require imagesloaded.pkgd.min
 //= require custom-js
-//= require jquery.turbolinks
